a1d479657619706e3284b10f26a783e7faee366c
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-12-04 02:15:06 -0800

    Got rid of counter reducer since we have it all in createSlice which is a method available in redux toolkit. Added configureStore method to be able to separate the store's reducer into individual slices.

--- redux-project/src/store/index.js
+++ redux-project/src/store/index.js
@@ -1,12 +1,11 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true
 };
 
-createSlice({
+const counterSlice = createSlice({
   name: 'general',
   initialState, // same as initialState: initialState
   reducers: {
@@ -31,38 +30,8 @@ export const DECREMENT = 'decrement';
 export const INCREMENTBY = 'incrementby';
 export const TOGGLE = 'toggle';
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === INCREMENT) {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === DECREMENT) {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === INCREMENTBY) {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === TOGGLE) {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;

a164180a389cff3a55d69d423e8fd0d6af2b71b4
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-12-04 02:07:19 -0800

    Installed redux toolkit and removed redux since redux toolkit already includes it so this is to prevent duplicate package. Used createSlice from redux toolkit and added all the reducers needed.

--- redux-project/src/store/index.js
+++ redux-project/src/store/index.js
@@ -1,9 +1,30 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true
 };
+
+createSlice({
+  name: 'general',
+  initialState, // same as initialState: initialState
+  reducers: {
+    increment(state) { // action not needed here
+      state.counter++; // here it is ok to manipulate the state since redux toolkit keeps track of the state behind the scenes.
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    incrementby(state, action) { // action needed here since we use it as additional payload.
+      state.counter = state.counter + action.amount
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter
+    }
+  }
+});
+
 
 export const INCREMENT = 'increment';
 export const DECREMENT = 'decrement';
